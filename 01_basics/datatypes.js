d49e5d73dc96e08b0670b2a872653a6ae22994ff
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2024-11-11 12:14:26 +0530

    Learning about Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+// for statndard documentation use (tc39.es or MDN documentation)
+
+// alert("Hello world");// it creates a popup message on your browser screen
+
+//Data types
+//primitive datatypes
+//number => 2 to the power 53 you can use
+//Bigint => use for large numeric values
+//string=>"" 
+//Boolean => ture/false
+//null=> standalore value;
+//undefined 
+//symbol
+
+//typeof =>use for identity the type of datatypes
+console.log(typeof "name");//output will be string
+//OR
+console.log(typeof 5);//out.. number
+//or many more
+
