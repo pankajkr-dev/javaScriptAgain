b1247cd04d672fc6d4d4b3826ed12b97b88c2803
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2024-11-12 05:01:52 +0000

    datatype conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,22 @@
+let score="33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+// let score="welcome"
+
+
+let valueInNumber=number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;0=>false
+
+let isLoggedIn="hello";
+
+let booleanIsLoggedIn=boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
