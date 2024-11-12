9ac0102034b9b810581c5f9705a7eae111a5dd3e
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2024-11-12 06:07:38 +0000

    operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,22 +1,38 @@
 let score="33";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 // let score="welcome"
 
 
-let valueInNumber=number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber=number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33"=>33
 //"33abc"=>NaN
 //true=>1;0=>false
 
-let isLoggedIn="hello";
+// let isLoggedIn="hello";
 
-let booleanIsLoggedIn=boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
+
+//***************operations************** */
+
+let value=3;
+
+let negValue=-value;
+console.log(negValue);
+
+//basics maths operatons
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2); //use for power(2 to the power 2);
+console.log(2%2); // for remender output
 
