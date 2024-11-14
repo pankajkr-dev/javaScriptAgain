55816a09670cb72c2e6d4be4897b2d7a02d0083b
PANKAJ KUMAR <pankajkr.vitj@gmail.com>
2024-11-14 13:27:36 +0000

    comparison of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -44,8 +44,21 @@ let str2=" Kumar";
 let str3=str1+str2;
 console.log(str3);
 
-console.log("2"+2);
-console.log(2+"2");
-console.log(1+"2"+"2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+// console.log("2"+2);
+// console.log(2+"2");
+// console.log(1+"2"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(true)// true
+console.log(+true) // possible
+// console.log(true+) // not possible
+console.log(+"");
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+let GameCounter=100;
+++GameCounter;
+console.log(GameCounter);
